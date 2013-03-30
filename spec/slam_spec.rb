96eeed9cd94a9884657f4b25ab85a1253c3aadd4
pasberth <pasberth@gmail.com>
2013-03-30 14:21:48 +0900

    テスト追加。

--- spec/slam_spec.rb
+++ spec/slam_spec.rb
@@ -20,4 +20,19 @@ describe Slam::Dunk do
     Hash[qstr.split(?&).map &l.split(?=)].
       should == {"hoge"=>"fuga", "foo"=>"bar"}
   end
+
+  example do
+    (1..5).map(&l ** 2).
+      should == [1, 4, 9, 16, 25]
+  end
+
+  example do
+    %w[c++ lisp].map(&(l + "er").upcase).
+      should == ["C++ER", "LISPER"]
+  end
+
+  example do
+    %w[c++ lisp].map(&l.upcase + "er").
+      should == ["C++er", "LISPer"]
+  end
 end
