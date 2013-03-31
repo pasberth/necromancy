93f8f69998eb9f0dd49078f8939531810b2938fa
pasberth <pasberth@gmail.com>
2013-04-01 00:37:36 +0900

    refinement でするより、サブクラスが賢いかも。

---
+++ lib/slam/base.rb
@@ -0,0 +1,35 @@
+module Slam
+
+  class Base < BasicObject
+
+    IDENTITY = ->(x) { x }
+
+    protected *instance_methods
+    protected
+
+    def initialize(callable = IDENTITY, *args, &block)
+      @callable = callable.to_proc
+      @args = args
+      @block = block
+    end
+
+    def method_missing(name, *args, &block)
+      self >> self.class.new(name, *args, &block)
+    end
+
+    def to_proc
+      ->(*args, &block) { @callable.(*args, *@args, &(block||@block)) }
+    end
+
+    def class
+      @class ||= (class << self; self end).superclass
+    end
+
+    def >>(callable)
+      f = to_proc
+      g = callable.to_proc
+      h = ->(*args, &block) { g.(f.(*args, &block)) }
+      self.class.new(h)
+    end
+  end
+end
