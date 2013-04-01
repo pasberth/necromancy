b8c183928f809acf30acd2bfa5a1e1eddf1ef42b
pasberth <pasberth@gmail.com>
2013-04-01 21:47:05 +0900

    Slam::Dunk を Slam::Compose に Identity を与えたものとするのは無理があった。。 nil で分岐する

--- lib/slam/dunk.rb
+++ lib/slam/dunk.rb
@@ -2,26 +2,34 @@ module Slam
 
   class Dunk < BasicObject
 
-    IDENTITY = ->(x) {x}
-
     protected *instance_methods
     protected
 
-    def initialize(callable = IDENTITY, *args, &block)
-      @callable = callable.to_proc
+    def initialize(callable = nil, *args, &block)
+      @callable = callable.to_proc unless callable.nil?
       @args = args
       @block = block
     end
 
-    def method_missing(name, *args, &block)
-      f = to_proc
-      g = self.class.new(name, *args, &block).to_proc
-      h = ->(*args, &block) { g.(f.(*args, &block)) }
-      self.class.new(h)
+    def compose(callable, *args, &block)
+      if @callable.nil?
+        self.class.new(callable, *args, &block)
+      else
+        f = to_proc
+        g = self.class.new(callable, *args, &block).to_proc
+        h = ->(*args, &block) { g.(f.(*args, &block)) }
+        self.class.new(h)
+      end
     end
 
+    alias method_missing compose
+
     def to_proc
-      ->(*args, &block) { @callable.(*args, *@args, &(block||@block)) }
+      if @callable.nil?
+        ->(x){x}
+      else
+        ->(*args, &block) { @callable.(*args, *@args, &(block||@block)) }
+      end
     end
 
     def class
