e6e9a9f0411575fb0f4bd9eda398a7d4edc43831
pasberth <pasberth@gmail.com>
2013-04-03 01:28:17 +0900

    ** とかは proc を返す proc を受け取る仕様なので fizzbuzz も fix

--- examples/fizzbuzz.rb
+++ examples/fizzbuzz.rb
@@ -7,7 +7,7 @@ else
 end
 
 L = Slam::Dunk.new
-puts (1..100).map &(L%15).zero? ** proc{"FizzBuzz"} |
-                   (L%3).zero?  ** proc{"Fizz"}     |
-                   (L%5).zero?  ** proc{"Buzz"}     |
+puts (1..100).map &(L%15).zero? ** proc{proc{"FizzBuzz"}} |
+                   (L%3).zero?  ** proc{proc{"Fizz"}}     |
+                   (L%5).zero?  ** proc{proc{"Buzz"}}     |
                    L
