a043e275a50605776b05ae0747583ecfea1431d5
pasberth <pasberth@gmail.com>
2013-04-03 01:43:48 +0900

    Slam::Alternative::Dunk.new とかできたら嬉しい！！

--- examples/fizzbuzz.rb
+++ examples/fizzbuzz.rb
@@ -1,12 +1,6 @@
 require 'slam'
 
-if RUBY_VERSION < "2.0.0"
-  class Slam::Dunk; include ::Slam::Alternative; end
-else
-  using Slam::Alternative
-end
-
-L = Slam::Dunk.new
+L = Slam::Alternative::Dunk.new
 puts (1..100).map &(L%15).zero? ** proc{proc{"FizzBuzz"}} |
                    (L%3).zero?  ** proc{proc{"Fizz"}}     |
                    (L%5).zero?  ** proc{proc{"Buzz"}}     |
--- lib/slam/ext.rb
+++ lib/slam/ext.rb
@@ -18,5 +18,14 @@ module Slam
         # ..
       end
     end
+
+    def const_missing(name)
+      mod = self
+      if name == :Dunk
+        Class.new(::Slam::Dunk) { include mod }
+      else
+        super
+      end
+    end
   end
 end
