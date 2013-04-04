aa0a736fdf08aea4908e15ded521ec9e6f8f27b7
pasberth <pasberth@gmail.com>
2013-04-04 09:56:39 +0900

    shared_examples_for にする

--- spec/alternative_spec.rb
+++ spec/alternative_spec.rb
@@ -9,29 +9,62 @@ describe AlternativeDunk do
   let(:l) { described_class.new }
 
   describe "#|" do
-    subject { proc(&l | proc{x}) }
-    let(:x) { 42 }
-    example { subject.(nil).should == x }
-    example { subject.(false).should == x }
-    example { subject.(true).should be_true }
+
+    shared_examples_for "hoge" do # TODO: hoge->???
+      subject { proc(&l | f) }
+      example { subject .(nil)   .should == f.() }
+      example { subject .(false) .should == f.() }
+      example { subject .(true)  .should == true }
+      example { subject .(0)     .should == 0    }
+      example { subject .([])    .should == []   }
+      example { subject .({})    .should == {}   }
+      example { subject .("")    .should == ""   }
+    end
+
+    context "42.itself" do
+
+      it_behaves_like "hoge" do
+        let(:f) { proc{42} }
+      end
+    end
   end
 
   describe "#*" do
-    subject { proc(&l.+ * proc{x}) }
 
-    context do
-      let(:x) { 42 }
-      example { subject.call(1).should == x + 1 }
+    shared_examples_for "pure" do
+      subject { proc(&l.f * g) }
+      example { subject.(r).should == r.f(g.()) }
     end
 
-    context do
-      let(:x) { nil }
-      example { subject.call(1).should == x }
+    shared_examples_for "empty" do
+      subject { proc(&l.f * g) }
+      example { subject.(r).should == g.() }
     end
 
+
     context do
-      let(:x) { false }
-      example { subject.call(1).should == x }
+      let(:r) { double }
+      before :each do
+        r.stub(:f, &:succ)
+      end
+
+      context do
+        it_behaves_like "pure" do
+          let(:g) { proc{42} }
+        end
+      end
+
+      context do
+        it_behaves_like "empty" do
+          let(:g) { proc{nil} }
+        end
+      end
+
+      context do
+        it_behaves_like "empty" do
+          let(:g) { proc{false} }
+        end
+      end
     end
   end
 
