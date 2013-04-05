1528655ac64bb0110166424ffab4dada61265ecd
pasberth <pasberth@gmail.com>
2013-04-05 09:45:12 +0900

    Category の仕様。

---
+++ spec/category_spec.rb
@@ -0,0 +1,25 @@
+require 'slam'
+
+class CategoryDunk < Slam::Dunk
+  include ::Slam::Category
+end
+
+describe CategoryDunk do
+
+  let(:l) { described_class.new }
+
+  shared_examples_for 'a Category' do
+
+    example { proc(&l > f).(r).should == proc(&f).(proc(&l).(r)) }
+    example { proc(&f < l).(r).should == proc(&f).(proc(&l).(r)) }
+    example { proc(&l > f > g).(r).should == proc(&g).(proc(&f).(proc(&l).(r))) }
+    example { proc(&g < f < l).(r).should == proc(&g).(proc(&f).(proc(&l).(r))) }
+  end
+
+  it_behaves_like "a Category" do
+
+    let(:r) { '42' }
+    let(:f) { described_class.new.to_i }
+    let(:g) { described_class.new + 1 }
+  end
+end
