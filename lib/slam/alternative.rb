d422aefe901f93561f795e532b206134f8c445be
pasberth <pasberth@gmail.com>
2013-04-06 22:10:57 +0900

    a | b | c | d のようなものは (((a | b) | c) | d) と 評価するより a | (b | (c | d)) と評価した方が、 無意味な条件の評価が行われないので高速である

--- lib/slam/alternative.rb
+++ lib/slam/alternative.rb
@@ -21,8 +21,22 @@ module Slam
 
     def |(callable)
       str = make_evaluable_string(callable)
-      necromancy = "self.empty?(*(xs = (#{@necromancy}))) ? (#{str}) : xs"
-      self.class.new(necromancy, @references.dup)
+
+      if @is_alternative_or
+        exprs = [str, *@exprs]
+      else
+        exprs = [str, @necromancy]
+      end
+
+      necromancy = exprs.inject do |else_expr, cond_expr|
+        "self.empty?(*(xs = (#{cond_expr}))) ? (#{else_expr}) : xs"
+      end
+
+      self.class.new(necromancy, @references.dup).instance_eval do
+        @is_alternative_or = true
+        @exprs = exprs
+        self
+      end
     end
   end
 end
