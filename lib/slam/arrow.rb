8d7160d8d3725640263da0b774eece3be6ae1bf5
pasberth <pasberth@gmail.com>
2013-04-07 00:02:46 +0900

    fix Arrow

--- lib/slam/arrow.rb
+++ lib/slam/arrow.rb
@@ -6,9 +6,8 @@ module Slam
   module Arrow; extend Ext
 
     def &(callable)
-      id = callable.to_proc.__id__
       str = make_evaluable_string(callable)
-      necromancy = "[*(#{@necromancy}), *(#{str})]"
+      necromancy = "(#{@necromancy}) + (#{str})"
       self.class.new(necromancy, @references.dup)
     end
   end
