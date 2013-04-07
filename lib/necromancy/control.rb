e69e2b2b40843e00f33058f7aa24d1d2c5dd21f2
pasberth <pasberth@gmail.com>
2013-04-07 18:49:37 +0900

    ドキュメント書く書く

--- lib/necromancy/control.rb
+++ lib/necromancy/control.rb
@@ -2,6 +2,8 @@ module Necromancy
 
   module Control
 
+    # Creates a {Necromancy::Necromancy} object including the {Necromancy::Control}.
+    # @return [Necromancy]
     def new
       mod = self
       Class.new(::Necromancy::Necromancy) { include mod }.new
@@ -14,6 +16,7 @@ module Necromancy
 
     private :branch
 
+    # @deprecated
     def call(*targets)
       warn <<EOF
 Necromancy.Hoge.() deprecated.
@@ -22,6 +25,7 @@ EOF
       branch { protected *instance_methods }[*targets]
     end
 
+    # @deprecated
     def using(*targets)
       warn <<EOF
 Necromancy.Hoge.using() deprecated.
@@ -30,7 +34,13 @@ EOF
       self[*targets]
     end
 
-    def [](*targets)
+    # Reveals some methods and Creates some aliases.
+    # @param [Symbol] target Reveals a method of the symbol.
+    # @param [Hash]   target Hides all methods of each key and creates aliases from each value to each key.
+    # @param [Array]  targets A list of Symbol or Hash that will be processing as target.
+    # @return [Control] Processed new {Necromancy::Control} object.
+    def [](target, *targets)
+      targets.unshift(target)
       names = targets.select { |t| t.is_a? Symbol }
       aliases = targets.select { |t| t.is_a? Hash }.inject(:merge) || {}
       branch do
@@ -43,7 +53,12 @@ EOF
       end
     end
 
-    def hiding(*names)
+    # Hides some methods.
+    # @param [Symbol] name Hides a method of the name.
+    # @param [Array]  names A list of a Symbol that will be processing as name.
+    # @return [Control] Processed new {Necromancy::Control} object.
+    def hiding(name, *names)
+      names.unshift(name)
       branch { protected *names }
     end
 
