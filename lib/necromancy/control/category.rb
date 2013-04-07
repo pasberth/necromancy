1be9c40f84265cff60e115df093d32d2b52d6ca8
pasberth <pasberth@gmail.com>
2013-04-07 19:08:32 +0900

    Category の例…

--- lib/necromancy/control/category.rb
+++ lib/necromancy/control/category.rb
@@ -5,12 +5,26 @@ module Necromancy
 
   module Control::Category; extend Control
 
+    # Left-to-right composition.
+    # self should be a -> b.
+    # @param [Object] callable b -> c
+    # @return [Necromancy] a -> c
+    # @example
+    #   lambda(&N.to_i > N * 2).('42').
+    #     should == '42'.to_i * 2
     def >(callable)
       str = make_evaluable_string(callable)
       necromancy = "args = (#{@necromancy}); #{str}"
       self.class.new(necromancy, @references.dup)
     end
 
+    # Right-to-left composition
+    # self should be b -> c.
+    # @param [Object] callable a -> b
+    # @return [Necromancy] a -> c
+    # @example
+    #   lambda(&N.to_i < N * 2).('42').
+    #     should == ('42' * 2).to_i
     def <(callable)
       str = make_evaluable_string(callable)
       necromancy = "args = (#{str}); #{@necromancy}"
--- spec/examples/category_spec.rb
+++ spec/examples/category_spec.rb
@@ -9,4 +9,14 @@ describe Necromancy::Control::Category do
     %w(0 1 2).map(&l.to_i > "foo".method(:[])).
       should == %w(0 1 2).map {|x| "foo"[x.to_i] }
   end
+
+  example do
+    lambda(&l.to_i > l * 2).('42').
+      should == '42'.to_i * 2
+  end
+
+  example do
+    lambda(&l.to_i < l * 2).('42').
+      should == ('42' * 2).to_i
+  end
 end
